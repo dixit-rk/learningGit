7981926f35f273d2596beda7a97507db86fbd5a9
Dixit Nakhva <dixitnakhva@gmail.com>
2024-05-09 16:12:48 +0530

    change 4,Wrapping Up

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 console.log("after creating file, change 1");
 console.log("change 2");
 console.log("change 3");
+console.log("learned about init,add . ,restore --staged,commit -m,stash,stash pop,stash clear,remote add,diff,status..Thank You..Tomorrow I'll Start With Branches");
